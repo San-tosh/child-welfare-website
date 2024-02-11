e3371c6a302ff2b84e8d175c583ca6cea02bcd57
San-tosh <bhuluns@yahoo.com>
2024-02-11 16:51:40 +0545

    new pages added for services and contact us

--- components/contact/main.component.tsx
+++ components/contact/main.component.tsx
@@ -48,7 +48,7 @@ const Main = ()=>{
                 <div className="col-md-6" id="map">
                     <iframe
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9895.670003809693!2d85.3483291569755!3d27.74170337922145!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb1bfac9183291%3A0xa0d797e00f59ad4b!2sKopan%20Monastery!5e0!3m2!1sen!2snp!4v1707641058067!5m2!1sen!2snp"
-                        width="600" height="450" style={{border:"0"}} allowFullScreen={false} loading="lazy"
+                        width="100%" height="100%" style={{border:"0"}} allowFullScreen={false} loading="lazy"
                         referrerPolicy="no-referrer-when-downgrade"></iframe>
                 </div>
             </div>
